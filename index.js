aa16990507258f583f85f7c3c5d076f611cb2961
ruwaidacoding123 <ruweidamuzamil075@gmail.com>
2023-02-13 05:13:39 -0800

    this is my project

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+let toggleButton = document.querySelector(".button");
+
+let links = document.querySelector(".links");
+
+
+
+toggleButton.addEventListener("click", function(){
+
+    links.classList.toggle("active");
+})
